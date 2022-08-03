adfc5daa184de4bb15e1be29a8ba9b841cedf2cb
okleebiz <okleebiz@gmail.com>
2022-08-03 11:17:26 +0900

    Push 1ck

---
+++ src/main/java/com/Wavic/SysD/WebWavicDController.java
@@ -0,0 +1,19 @@
+package com.Wavic.SysD;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebWavicDController {
+	@RequestMapping("")
+	public String welcome() {
+		System.out.println("########welcome#################");
+		return "welcome.jsp";
+	}
+	
+	@RequestMapping("test")
+	public String test() {
+		System.out.println("########test#################");
+		return "test.jsp";
+	}
+}
---
+++ src/test/java/com/Wavic/SysD/WebWavicDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wavic.SysD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWavicDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
